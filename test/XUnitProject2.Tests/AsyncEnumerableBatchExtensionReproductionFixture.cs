c8082d2260f3a05e981a5a4545fab569e4b7ea4f
Bert <bert.k@hotmail.com>
2026-05-08 16:53:29 +0200

    Add AsyncEnumerableBatchExtensionReproductionFixture and update package versions (#56)
    * Add AsyncEnumerableBatchExtensionReproductionFixture and update package versions
    
    * Refactor AsyncEnumerableBatchExtensionReproductionFixture to use await foreach for batch processing

---
+++ test/XUnitProject2.Tests/AsyncEnumerableBatchExtensionReproductionFixture.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClassLibrary2;
+using Xunit;
+
+namespace XUnitProject2.Tests;
+
+public class AsyncEnumerableBatchExtensionReproductionFixture
+{
+    [Fact]
+    public async Task ExecuteReproduction_UncoveredBranches()
+    {
+        // Arrange
+        var enumerable = AsyncEnumerable.Range(1, 95);
+
+        // Act
+        IAsyncEnumerable<IAsyncEnumerable<int>> batches = enumerable.ExecuteReproduction(10);
+         await foreach (IAsyncEnumerable<int> batch in batches)
+         {
+             await batch.ToArrayAsync();
+         }
+
+        // Assert
+        // no assert
+    }
+}
